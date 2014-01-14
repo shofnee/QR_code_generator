3eb39ab3cd2f969c2206bf9560c48d4d9606a289
shofnee <shofnee@gmail.com>
2014-01-14 15:36:24 -0800

    Update generator.php

--- generator.php
+++ generator.php
@@ -1,4 +1,15 @@
 <?php
+//data variables
+
+//qr code image height & width
+$w = '150';
+$h = '150';
+
+$data = 'this is my email info@shofnee.com';
+
+//this will give you a 150 by 150 qr code for the data saved in the ($data) variable
+echo 'https://chart.googleapis.com/chart?cht=qr&chl='.$data.'&chs='.$w.'x'.$h.';
+
 
 
 ?>
